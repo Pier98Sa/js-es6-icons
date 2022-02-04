6d1846ca53ace0e032d32ddb9a354a8ca7a4f076
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-02-04 19:35:15 +0100

    creata la funzione che crea il colore dinamicamente

---
+++ bonus/main.js
@@ -0,0 +1,184 @@
+const icons = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
+//FUNZIONI
+function drawIcons(container, icons){
+    let content = '';
+
+    icons.forEach(item => {
+        content += `
+                    <div class="icon">
+                        <div class="item flex-center">
+                            <i style= "color:${item.color};" class="${item.family}  ${item.prefix}${item.name}"></i>
+                            <div class="item-text">
+                                <span>${item.name}</span>
+                            </div>
+                        </div>
+                    </div>
+                    `;
+    });
+
+
+    container.innerHTML = content;
+
+}
+//funzione che crea un colore in esadecimale
+function makeColor(length) {
+	//inizializzo la variabile di uscita con il cancelletto
+	let result = '#';
+	//creo una stringa dei valori accettati 
+	let characters = 'abcdef0123456789';
+	//creo un ciclo
+	for ( let i = 0; i < length; i++ ) {
+		//utilizzo la funzione charAt che restituisce un carattere da una stringa dalla posizione indicata dall'indice
+		//per rendere la scelta randomica il numero viene generato tramite la math.random
+	   result += characters.charAt(Math.floor(Math.random() * characters.length));
+	}
+	return result;
+ }
+ console.log(makeColor(6));
+//Costanti
+
+const container = document.getElementById('icons-container');
+const selector = document.getElementById('type-filter');
+
+drawIcons(container, icons);
+//ascoltatore di eventi
+selector.addEventListener('change', function () {
+    
+    let selection = this.value;
+
+    //if per far si che quando il filtro è su all compaiano tutti
+    if(selection == ""){
+
+        drawIcons(container, icons);
+
+    }else{
+        //filtro che fa comparire solo le icone del tipo giusto
+        const filtered = icons.filter(icon => {
+            if(icon.type == selection){
+                return true;
+            }
+            return false;
+        });
+
+        drawIcons(container, filtered);
+    
+    }
+    
+});
+
+
+
+
